8fe3301acc10e878a92c097a47b036fd1da49394
Emily Mearns <mearnsem@gmail.com>
2016-06-13 09:07:39 -0600

    Stretch Problem Initial Commit

---
+++ StretchProblem/FuzzySearch/FuzzySearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FuzzySearch
+//
+//  Created by Emily Mearns on 6/13/16.
+//  Copyright © 2016 Emily Mearns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
